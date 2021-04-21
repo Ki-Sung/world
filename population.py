fda8b39f73294cbcbe039165ac38cd18c2e15ef5
Ubuntu <ubuntu@ip-172-31-35-208.ap-northeast-2.compute.internal>
2021-04-21 10:54:46 +0900

    init world

---
+++ population.py
@@ -0,0 +1,13 @@
+import configparser
+import MySQLdb
+import pandas as pd
+
+config = configparser.ConfigParser()
+config.read("../datas.ini)
+datas = config["dss"]
+
+connect_datas = {"host": datas["PUBLIC_ID"], "user": "root", "passwd": "dss",
+                 "db": "world", "charset": "utf8"}
+client = client = MySQLdb.connect(**connect_datas)
+df = pd.read_sql("SELECT code, name, population FROM country", client)
+print(df.tail())
